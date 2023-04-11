bac9ee4714cb7793075af89055d93fde63b1b4ca
AnibalArguelles <anibal_arguelles_90@hotmail.com>
2023-04-11 12:09:22 -0600

    microservice update with matters

---
+++ src/test/java/com/tec/alumnos/materias/MateriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec.alumnos.materias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
